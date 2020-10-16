73c9de66271d550f9de867649cb1cc606f8e9c38
sveinn31 <72951716+sveinn31@users.noreply.github.com>
2020-10-15 22:49:57 -0400

    Add tangent line
    added tangent line to existing graph

--- pyrefresher.py
+++ pyrefresher.py
@@ -5,8 +5,12 @@ def func():
     x = np.arange(0,2*np.pi,0.1)   
     y = np.sin(x)
     z = np.cos(x)
-    plt.plot(x,y,x,z)
-    plt.show()
+    #plt.plot(x,y,x,z)
+    #plt.show()
+    t = np.arange(0.0, 1, 0.01)
+    s = np.tan(t)
+    plot.plot(x,y,x,z,t,s)
+    plot.show()
 
 if __name__ == "__main__":
     func()
